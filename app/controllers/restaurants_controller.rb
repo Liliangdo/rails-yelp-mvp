111366b0ea915d24d6f86ebdf2b56c1d6ac6d134
lilian garrido <lilian.garrido13@gmail.com>
2016-08-10 16:47:12 +0200

    restaurant controller and views plus routes done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,20 +4,27 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
+
   end
 
   def create
+    @restaurant = Restaurant.new(restaurants_params)
+    if @restaurant.save
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
 
-  def update
-  end
+  private
 
-  def destroy
+  def restaurants_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
